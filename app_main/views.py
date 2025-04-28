4b97b485baa8d735784049a17ac6dfcf8eb3a39b
Aayush Dangol <ayushazhar@gmail.com>
2025-04-28 17:55:26 +0545

    test(db): add unit test for model relationship

--- app_main/views.py
+++ app_main/views.py
@@ -4,15 +4,20 @@ from app_main.models import *
 def home(request):
     province_name=request.GET.get('province')
     district_name=request.GET.get('district')
+    localunit_name=request.GET.get('localunit')
+    
     parties=Party.objects.all()
     province=Province.objects.all()
     district_=District.objects.filter(province=province_name)
     Local_unit_=Local_unit.objects.filter(district=district_name)
+    candidate=Candidate.objects.filter(local_unit=localunit_name)
+    
     context={
         'parties':parties,
         'province':province,
         'district_':district_,
-        'Local_unit_':Local_unit_
+        'Local_unit_':Local_unit_,
+        'candidate':candidate
     }
     
     return render(request, 'app_main/home.html',context=context)
