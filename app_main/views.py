8c77d48e55bf25b39273df033b74e9b954d9e833
Aayush Dangol <ayushazhar@gmail.com>
2025-04-28 17:35:28 +0545

    test(model): db-test

--- app_main/views.py
+++ app_main/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from app_main.models import *
 # Create your views here.
 def home(request):
-    return render(request, 'app_main/home.html')
+    province_name=request.GET.get('province')
+    district_name=request.GET.get('district')
+    parties=Party.objects.all()
+    province=Province.objects.all()
+    district_=District.objects.filter(province=province_name)
+    Local_unit_=Local_unit.objects.filter(district=district_name)
+    context={
+        'parties':parties,
+        'province':province,
+        'district_':district_,
+        'Local_unit_':Local_unit_
+    }
+    
+    return render(request, 'app_main/home.html',context=context)
