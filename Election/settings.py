7e109b1ed7cb7fc3cca6a8b1492ab4ac510c904c
Isha0711 <ishapant16@gmail.com>
2025-04-28 21:30:45 +0545

    Chief Panel results updated

--- Election/settings.py
+++ Election/settings.py
@@ -119,5 +119,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[STATIC_DIR]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
