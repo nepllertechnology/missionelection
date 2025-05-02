335e321075a0f34f18657bd72bb60f041ccb942f
dipesh0078 <thapadipesh2058@gmai.com>
2025-05-03 00:10:26 +0545

    connected

--- app_main/urls.py
+++ app_main/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from app_main import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
      path('api/provinces/', views.get_provinces),
     path('api/districts/', views.get_districts),  # Use province_name in URL
     path('api/municipalities/', views.get_municipalities),
+    path('search/', views.search_results, name='search_results'),
+    path('candidate_list_ajax/', views.candidate_list_ajax, name='candidate_list_ajax'),
 ]
--- app_main/views.py
+++ app_main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from app_main.utils import metroAndsubmetro,partyResult
-
 from .models import *
+from collections import defaultdict
+from django.shortcuts import redirect
 
 # Home view remains the same
 def home(request):
@@ -30,5 +31,57 @@ def get_municipalities(request):
     municipalities = Local_unit.objects.filter(district_id=district_name).values('name')  # Filter using district_name
     return JsonResponse(list(municipalities), safe=False)
 
+def search_results(request):
+    province_name = request.GET.get('province')
+    district_name = request.GET.get('district')
+    municipality_name = request.GET.get('municipality')
 
+    results = None
+    result_type = None  # <-- to track what kind of data we are returning
 
+    if municipality_name:
+        results = Local_unit.objects.filter(pk=municipality_name)
+        result_type = 'local_unit'
+    elif district_name:
+        results = District.objects.filter(pk=district_name)
+        result_type = 'district'
+    elif province_name:
+        results = Province.objects.filter(pk=province_name)
+        result_type = 'province'
+
+    provinces = Province.objects.all()
+    districts = District.objects.all()
+    municipalities = Local_unit.objects.all()
+
+    context = {
+        'results': results,
+        'result_type': result_type,  
+        'provinces': provinces,
+        'districts': districts,
+        'municipalities': municipalities,
+    }
+    return render(request, 'candidate.html', context)
+
+def candidate_list_ajax(request):
+    print("triggerr")
+    local_unit_name = request.GET.get('local_unit')
+    ward_number = request.GET.get('ward')
+
+    try:
+        local_unit = Local_unit.objects.get(name=local_unit_name)
+    except Local_unit.DoesNotExist:
+        return JsonResponse({'error': 'Local unit not found'}, status=404)
+
+    candidates = Candidate.objects.filter(local_unit=local_unit, ward=ward_number).order_by('-vote')
+
+    grouped_candidates = defaultdict(list)
+    for candidate in candidates:
+        grouped_candidates[candidate.position].append({
+            'name': candidate.name,
+            'photo': candidate.photo.url,
+            'party': candidate.party.party_name,
+            'position': candidate.position,
+            'vote': candidate.vote,
+        })
+
+    return JsonResponse({'grouped_candidates': grouped_candidates})
